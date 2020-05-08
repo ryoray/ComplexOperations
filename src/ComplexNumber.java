f1e83c4b348b6182bb98f7fc568e3b9e46a1bf8b
ryoray <praretno.wibowo@gmail.com>
2020-05-09 05:47:41 +0700

    Initial commit

---
+++ src/ComplexNumber.java
@@ -0,0 +1,38 @@
+public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add (double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void subtract (double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void add (ComplexNumber number) {
+        this.real += number.getReal();
+        this.imaginary += number.getImaginary();
+    }
+
+    public void subtract (ComplexNumber number) {
+        this.real -= number.getReal();
+        this.imaginary -= number.getImaginary();
+    }
+}
